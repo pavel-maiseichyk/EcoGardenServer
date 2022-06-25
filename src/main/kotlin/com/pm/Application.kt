27cd586b70996db0edc7ca6a94a7e0f7771fa086
pavel_maiseichyk <pavel.maiseichyk@gmail.com>
2022-06-25 12:16:49 +0200

    Initial Commit

---
+++ src/main/kotlin/com/pm/Application.kt
@@ -0,0 +1,17 @@
+package com.pm
+
+import io.ktor.server.application.*
+import com.pm.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+//    configureKoin()
+//    configureAuth()
+    configureRouting()
+//    configureSerialization()
+    configureMonitoring()
+  // configureSession()
+}
