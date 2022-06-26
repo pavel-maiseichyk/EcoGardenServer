80b7fe6310dcb8eb13877a17301bef86eb78d2f9
pavel_maiseichyk <pavel.maiseichyk@gmail.com>
2022-06-26 15:12:20 +0200

    Koin Module updated

--- src/main/kotlin/com/pm/di/KoinModule.kt
+++ src/main/kotlin/com/pm/di/KoinModule.kt
@@ -8,7 +8,7 @@ import org.litote.kmongo.reactivestreams.KMongo
 
 val koinModule = module {
     single {
-        KMongo.createClient()
+        KMongo.createClient(System.getenv("MONGODB_URI"))
             .coroutine
             .getDatabase("user_database")
     }
